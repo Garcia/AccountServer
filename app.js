819a398dc7a4d6f0d7270f9f9ab251a549058565
Garcia Souza <garcia.rsouza@gmail.com>
2012-08-08 16:48:34 -0300

    add cluster func

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var restify = require('restify'),
   util = require('util'),
+  cluster = require('cluster'),
+  numCpus = require('os').cpus().length,
   mongo = require('mongoose'),
   db = mongo.connect('mongodb://localhost/account_server'),
   schema = mongo.Schema,
@@ -12,27 +14,33 @@ var restify = require('restify'),
   CreditCard = models.CreditCard,
   User = models.User;
 
-// server config & listener's
+if (cluster.isMaster) {
+  for (var i = 0; i < (process.env['APP_FORK'] || numCpus); i++) {
+    cluster.fork();
+  }
+} else {
+  // server config & listener's
 
-var server = restify.createServer({
-  name: 'Account Server'
-});
+  var server = restify.createServer({
+    name: 'Account Server'
+  });
 
-server.use(restify.bodyParser());
+  server.use(restify.bodyParser());
 
-contrl.mapResource(server, User, ['user'], '/users', '/users/:user');
+  contrl.mapResource(server, User, ['user'], '/users', '/users/:user');
 
-contrl.mapResource(server, Email, ['owner', 'mail'],
-  /^\/users\/(.+)\/emails/, /^\/users\/(.+)\/emails\/(.+)/);
+  contrl.mapResource(server, Email, ['owner', 'mail'],
+    /^\/users\/(.+)\/emails/, /^\/users\/(.+)\/emails\/(.+)/);
 
-contrl.mapResource(server, Phone, ['owner', 'area', 'number'],
-  /^\/users\/(.+)\/phones/, /^\/users\/(.+)\/phones\/(.+)\/(.+)/);
+  contrl.mapResource(server, Phone, ['owner', 'area', 'number'],
+    /^\/users\/(.+)\/phones/, /^\/users\/(.+)\/phones\/(.+)\/(.+)/);
 
-contrl.mapResource(server, Address, ['owner', '_id'],
-  /^\/users\/(.+)\/address/, /^\/users\/(.+)\/address\/(.+)/);
+  contrl.mapResource(server, Address, ['owner', '_id'],
+    /^\/users\/(.+)\/address/, /^\/users\/(.+)\/address\/(.+)/);
 
-contrl.mapResource(server, CreditCard, ['owner', 'number'],
-  /^\/users\/(.+)\/creditcards/, /^\/users\/(.+)\/creditcards\/(.+)/);
+  contrl.mapResource(server, CreditCard, ['owner', 'number'],
+    /^\/users\/(.+)\/creditcards/, /^\/users\/(.+)\/creditcards\/(.+)/);
 
-server.listen((process.env['APP_PORT'] || 8000));
+  server.listen((process.env['APP_PORT'] || 8000));
+}
 
